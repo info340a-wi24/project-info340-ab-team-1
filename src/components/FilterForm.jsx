9ccc20a87bfd58a6b76c871970d9ac9bb8f92ff1
alex-labbe <alex.labbe@outlook.com>
2024-02-29 18:56:05 -0800

    firebase works, and removed a lot of files we no longer need

---
+++ src/components/FilterForm.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function FilterForm() {
+    return (
+        <form>
+            <div>
+            <label for="bike-rack-type">Bike rack type:</label>
+            <input type="radio" id="covered" name="bike-rack-type" value="covered"/>
+            <label for="covered">Covered</label>
+            <input type="radio" id="uncovered" name="bike-rack-type" value="uncovered"/>
+            <label for="uncovered">Uncovered</label>
+            <input type="radio" id="locked" name="bike-rack-type" value="locked"/>
+            <label for="locked">Locked</label>
+            </div>
+
+            <div>
+                <label for="bike-location">Bike location:</label>
+                <input type="radio" id="on-campus" name="bike-location" value="on-campus"/>
+                <label for="on-campus">On campus</label>
+                <input type="radio" id="off-campus" name="bike-location" value="off-campus"/>
+                <label for="off-campus">Off campus</label>
+            </div>
+
+            <div>
+                <label for="has-charger">Access to an outlet:</label>
+                <input type="radio" id="has-outlet" name="has-charger" value="has-outlet"/>
+                <label for="has-outlet">Has outlet</label>
+            </div>
+
+            <div>
+            <input type="submit" value="Submit"/>
+            </div>
+      </form>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI9b_vxobodkZJsq2QcHT2xTF7DF9qXu8",
+  authDomain: "bike-locator-info340.firebaseapp.com",
+  projectId: "bike-locator-info340",
+  storageBucket: "bike-locator-info340.appspot.com",
+  messagingSenderId: "10640855230",
+  appId: "1:10640855230:web:d4177df4457c77b5f08eef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
