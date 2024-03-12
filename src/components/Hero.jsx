7aa327f788e17dbd82d223232a7e1bd5af92691b
Jaiden1 <Jaiden1@uw.edu>
2024-03-11 17:10:50 -0700

    edited hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,9 @@ export default function Hero() {
         <div className="heroContainer">
             <div className="heroContent">
                 <h1>Unlock Your Adventure: Find Bike Racks Near You</h1>
-                <p>Seamless rides, hassle-free parking, every time.</p>
+                <p>Seamless rides, hassle-free parking, every time.
+                Our innovative app helps you locate bike racks effortlessly, ensuring your rides are seamless from start to finish.
+                With thousands of satisfied users, we've revolutionized urban cycling, making it easier and more enjoyable than ever.</p>
                 <button className="heroButton">Get Started</button>
             </div>
             <img className="heroImage" src="bikes.png" alt="A person riding a bike in Seattle in monochromatic theme, with the Space Needle, Mt Rainier, and the Seattle skyline in the background"/>
